7515fa75286b8df4facdc01758752b49094d2356
Walid Amar <walidamarpro@gmail.com>
2022-04-18 01:53:32 -0400

    3 todos

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import Todo from './Todo';
+import {Todo1, Todo2, Todo3} from './Todo';
 
 function App() {
   return (
     <div class="todo-container">
-      <Todo />
+      <Todo1 />
+      <Todo2 />
+      <Todo3 />
     </div>
   );
 }
--- src/Todo.js
+++ src/Todo.js
@@ -1,7 +1,7 @@
 import { Modal } from './Modal';
 import {useState} from 'react'
 
-export const Todo = () => {
+export const Todo1 = () => {
     const [showModal, setShowModal] = useState(false);
 
     const onDismissModal = () => {
@@ -19,6 +19,52 @@ export const Todo = () => {
             { showModal && <Modal dismissModal={onDismissModal} />}
         </>
     );
+}
+
+export const Todo2 = () => {
+    const [showModal, setShowModal] = useState(false);
+
+    const onDismissModal = () => {
+        setShowModal(false);
+    }
+
+    return (
+        <>
+            <div class="card">
+                <div class="card-content">
+                    <h2>Workout</h2>
+                    <button onClick={() => setShowModal(true)} class="btn">Done</button>
+                </div>
+            </div>
+            { showModal && <Modal dismissModal={onDismissModal} />}
+        </>
+    );
+}
+
+export const Todo3 = () => {
+    const [showModal, setShowModal] = useState(false);
+
+    const onDismissModal = () => {
+        setShowModal(false);
+    }
+
+    return (
+        <>
+            <div class="card">
+                <div class="card-content">
+                    <h2>Sleep Early</h2>
+                    <button onClick={() => setShowModal(true)} class="btn">Done</button>
+                </div>
+            </div>
+            { showModal && <Modal dismissModal={onDismissModal} />}
+        </>
+    );
+}
+
+const Todo ={
+    Todo1,
+    Todo2,
+    Todo3,
 }
 
 export default Todo;
